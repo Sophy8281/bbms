660b80e73962765efaa17258e971025e53f6c050
sophy8281 <wanjikusophia3@gmail.com>
2021-04-18 18:44:29 +0300

    created admin blood stock view

---
+++ resources/views/admin/stock/blood.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.admin_dashboard')
+
+@section('content')
+
+<div class="panel panel-default">
+    <div class="panel-heading"><h5>Whole Blood in Stock </h5></div>
+    <div class="panel-body">
+        <div class="col-md">
+            <table id="example" class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>#Id</th>
+                        <th>Donation ID</th>
+                        <th>Bank</th>
+                        <th>Staff</th>
+                        <th>Bag Serial No.</th>
+                        <th>B.Group</th>
+                        <th>Donation Date</th>
+                        <th>Expiry</th>
+                        <th>Days Remaining</th>
+                        {{-- <th>Actions</th> --}}
+                    </tr>
+                </thead>
+                <tbody>
+
+                    @forelse($bloods as $blood)
+                    <tr>
+                        <td>{{ $blood->id }}</td>
+                        <td>{{ $blood->donation_id }}</td>
+                        <td>{{ $blood->bank->name }}</td>
+                        <td>{{ $blood->staff->name }}</td>
+                        <td>{{ $blood->bag_serial_number }}</td>
+                        <td>{{ $blood->group->name }}</td>
+                        <td>{{ date('F d, Y', strtotime($blood->donation_date)) }}</td>
+                        <td>{{ date('F d, Y', strtotime($blood->expiry_date)) }}</td>
+                        @if ($blood->expiry_date == Carbon\Carbon::today()|$blood->expiry_date < Carbon\Carbon::today())
+                        <td>
+                            <a href="" class="btn btn-warning"> EXPIRED</a>
+                        </td>
+                        {{-- <td>
+                            <a href="{{ url('staff/blood/discard/'.$blood->id) }}" class="btn btn-danger"> Discard</a>
+                        </td> --}}
+                    @else
+                        <td>{{ Carbon\Carbon::create($blood->expiry_date)->diffInDays(Carbon\Carbon::today())}}</td>
+                        {{-- <td>
+                            <a href="{{ url('staff/blood/issue/'.$blood->id) }}" class="btn btn-success"> Issue</a>
+                        </td> --}}
+
+                    @endif
+                    </tr>
+                    @empty
+                    <tr>
+                        <td colspan="9">No blood in cold rooms.</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('javascript')
+<!-- export Scripts -->
+<script>
+    $(document).ready(function(){
+        $('#example').DataTable({
+            pageLength: 25,
+            responsive: true,
+            paging:true,
+            // searching:false,
+            dom: '<"html5buttons"B>lTfgitp',
+            buttons: [
+                {extend: 'copy', className: 'btn btn-outline-primary btn-lg', text:'<i class="fa fa-copy"></i>' },
+                {extend: 'csv', className: 'btn btn-primary btn-outline btn-lg',text:'<i class="fa fa-file-csv"></i>'},
+                {extend: 'excel', title: 'Donors', className: 'btn btn-primary btn-outline btn-lg', text:'<i class="fa fa-file-excel"></i>'},
+                {extend: 'pdf', title: 'Donors',  className: 'btn btn-primary btn-outline btn-lg', text:'<i class="fa fa-file-pdf"></i>'},
+                {extend: 'print', className: 'btn btn-primary btn-outline btn-lg', text:'<i class="fa fa-print"></i>',
+                    customize: function (win){
+                    $(win.document.body).addClass('white-bg');
+                    $(win.document.body).css('font-size', '10px');
+
+                    $(win.document.body).find('table')
+                    .addClass('compact')
+                    .css('font-size', 'inherit');
+                }
+                }
+            ]
+        });
+    });
+
+</script>
+@endsection

8afa3e42dea72463ea5eed622e65510f5f11474e
sophy8281 <wanjikusophia3@gmail.com>
2021-04-19 20:44:23 +0300

    tested mustverifyemail on user model

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,6 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
+    // protected $redirectTo = '/email/verify';
     protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,8 +20,8 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        // $this->middleware('auth','verified');
         $this->middleware('auth');
+        // $this->middleware('verified');
     }
 
     /**
